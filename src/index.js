ee9f7e9cc36bdcbf06850a40c26098dd8df105a8
Phlegm Mignon <dmah@ucdavis.edu>
2022-07-22 02:38:00 -0700

    Progress on task cards and popup

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import './style.css';
 function component() {
     let element = contentGen();
 
-
-    let main = rightGen();
-
     //How do I link the left project and it's list items on the right gen if they're 
     //in 2 different generators? I should add function in the factory when called it'll
     //add a list item to the object
@@ -53,7 +50,7 @@ function contentGen() {
 
     let defaultProject = projectFactory('First Project', projectContainer);
 
-    const popup = makeProjectDOM(sidebar);
+    const popup = makePopupDOM(sidebar, projectContainer, 'makeProject');
     
     const addProjectBtn = document.createElement('button');
     addProjectBtn.id ='addProjectBtn';
@@ -64,10 +61,15 @@ function contentGen() {
     //Click on each project changes main
 
 
+    //Creates right side
+    let main = document.createElement('div');
+    main.id = 'main';
+    taskGen(defaultProject, main);
 
 
 
     content.appendChild(sidebar);
+    content.appendChild(main);
     return content
     
 
@@ -78,8 +80,10 @@ function contentGen() {
         thing.classList.add('projects');
         const taskList = [];
     
-        const addTask = () => {
-            
+        let taskID = 0;
+        const addTask = (title, details, dueDate, priority, taskID) => {
+            let task = makeTaskDOM();
+            container.appendChild(task);
         }
 
         
@@ -88,55 +92,7 @@ function contentGen() {
         return {title, taskList, addTask};
     }
 
-    //sidebar stuff
-    function makeProjectDOM(sidebar) {
-        const popup = document.createElement('DIALOG');
-        popup.id = 'popup';
-
-        const popupHeader = document.createElement('div');
-        popupHeader.id = 'popupHeader';
-        popupHeader.innerHTML = 'New Project';
-
-        
-        const popupForm = document.createElement('FORM');
-        popupForm.id = 'popupForm';
-        popupForm.method = 'get';
-        popupForm.action = './index.html';
-        const textarea = document.createElement('TEXTAREA');
-        textarea.id = 'textarea';
-        popupForm.appendChild(textarea);
-
-        const popupContainer = document.createElement('div');
-        popupContainer.id = 'popupContainer';
-
-        const submitBtn = document.createElement('button');
-        submitBtn.type = 'submit';
-        submitBtn.id = 'submitBtn';
-        submitBtn.innerHTML = 'Submit';
-
-        const closeBtn = document.createElement('button');
-        closeBtn.type = 'button';
-        closeBtn.id = 'closeBtn';
-        closeBtn.innerHTML = 'Cancel';
-        closeBtn.addEventListener('click', () => {popup.close()});
-
-        popupForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            console.log(textarea.value);
-            projectFactory(textarea.value, projectContainer);
-            //might need counter 
-            // counter++;
-        })
-        
-        popup.appendChild(popupHeader);
-        popup.appendChild(popupForm);
-        popupContainer.appendChild(submitBtn);
-        popupContainer.appendChild(closeBtn);
-        popupForm.appendChild(popupContainer);
-        sidebar.appendChild(popup);
-
-        return popup
-    }
+    
 
 
 
@@ -145,9 +101,28 @@ function contentGen() {
 
 }
 
-function rightGen() {
-    let element = document.createElement('div');
-    element.id = 'main';
+function taskGen(project, main) {
+    let rightHeader = document.createElement('div');
+    rightHeader.id = 'rightHeader';
+    rightHeader.innerHTML = 'Task List';
+
+    let taskContainer = document.createElement('div');
+    taskContainer.id = 'taskContainer';
+
+    const popup = makePopupDOM(main, taskContainer, 'makeTask');
+    let defaultTask = project.addTask('Default task', '', '7.24.22', 'low');
+
+    let addTask = document.createElement('div');
+    addTask.id = 'addTask';
+    addTask.innerHTML = 'Add a new task';
+    addTask.addEventListener('click', () => popup.showModal());
+
+    
+    // addTask.addEventListener('click', )
+
+
+
+    
     
     //Default project opened
     //Task gen DOM + factory
@@ -157,12 +132,107 @@ function rightGen() {
 
     //Add task function
 
-    return element
+    main.appendChild(rightHeader);
+    main.appendChild(taskContainer);
+    main.appendChild(addTask);
+    
+    return main
 }
 
+//sidebar stuff
+function makePopupDOM(side ,container, format) {
+    const popup = document.createElement('DIALOG');
+
+    const popupHeader = document.createElement('div');
+    popupHeader.id = 'popupHeader';
+
+    
+    const popupForm = document.createElement('FORM');
+    popupForm.method = 'get';
+    popupForm.action = './index.html';
+    const textarea = document.createElement('TEXTAREA');
+    textarea.classList.add('textarea');
+    popupForm.appendChild(textarea);
+
+    const popupContainer = document.createElement('div');
+
+    const submitBtn = document.createElement('button');
+    submitBtn.type = 'submit';
+    submitBtn.innerHTML = 'Submit';
+    submitBtn.classList.add('submitBtn');
+
+    const closeBtn = document.createElement('button');
+    closeBtn.type = 'button';
+    closeBtn.innerHTML = 'Cancel';
+    closeBtn.classList.add('closeBtn');
+    closeBtn.addEventListener('click', () => {popup.close()});
+
+    popup.appendChild(popupHeader);
+    popup.appendChild(popupForm);
+    popupContainer.appendChild(submitBtn);
+    popupContainer.appendChild(closeBtn);
+    popupForm.appendChild(popupContainer);
+    
+    if (format == 'makeProject') {
+        popup.id = 'projectPopup';
+        popupHeader.innerHTML = 'New Project';
+        popupForm.id = 'projectPopupForm';
+        popupContainer.id = 'projectPopupContainer';
+        // submitBtn.id = 'projectSubmitBtn';
+        // closeBtn.id = 'projectCloseBtn';
+
+        popupForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            console.log(textarea.value);
+            projectFactory(textarea.value, container);
+            //might need counter 
+            // counter++;
+        })
+
+        side.appendChild(popup);
+    }
+    else if (format =='makeTask') {
+        popup.id = 'taskPopup';
+        popupHeader.innerHTML = 'New Task';
+        popupForm.id = 'taskPopupForm';
+        popupContainer.id = 'taskPopupContainer';
+        // submitBtn.id = 'taskSubmitBtn';
+        // closeBtn.id = 'taskCloseBtn';
+
+        popupForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            console.log(textarea.value);
+            projectFactory(textarea.value, projectContainer);
+            //might need counter 
+            // counter++;
+        })
+
+        side.appendChild(popup);
+    }
+
+    return popup
+}
+
+function makeTaskDOM(title, details, dueDate, priority, taskID) {
+    const taskCard = document.createElement('div');
+    taskCard.id = taskID;
+
+    const taskPriority = document.createElement('div');
+
+    const taskText = document.createElement('div');
+    taskText.innerHTML = title;
+
+    const taskDetails = document.createElement('div');
+
+    const taskDetailsBtn = document.createElement('button');
+
+    const taskPriorityBtn = document.createElement('button');
 
 
+     
 
+    return taskCard
+}
 
 
 
